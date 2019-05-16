65040e13c5c549e9a0198ac8e9351379ec10519e
Ben Loper <loperbp@gmail.com>
2019-05-15 20:52:28 -0400

    Finish Vowel Count challenge

---
+++ VowelCount/VowelCount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VowelCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVowelCount("test"));
+            Console.ReadKey();
+        }
+
+        static int GetVowelCount(string str)
+        {
+            int count = 0;
+
+            HashSet<char> vowels = new HashSet<char>
+            {
+                'a', 'e', 'i', 'o', 'u'
+            };
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (vowels.Contains(str[i]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
